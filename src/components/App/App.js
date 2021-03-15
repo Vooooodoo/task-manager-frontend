6f5fccc4da28e78e94f41e4b48a90dee951b4682
FusionTech <fusion@fusion.com>
2021-03-15 15:04:35 +0300

    feature: add global style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,13 @@
+import Button from '@material-ui/core/Button';
+import GlobalStyle from '../GlobalStyle';
+
 function App() {
   return (
     <>
-    
+      <GlobalStyle />
+      <Button size='large' variant='contained' color='primary'>
+        Hello World
+      </Button>
     </>
   );
 }
