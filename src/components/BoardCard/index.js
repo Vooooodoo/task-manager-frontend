e6f220bdb6a4cc9c5f792e11a6339156da02c2de
FusionTech <fusion@fusion.com>
2021-03-24 14:53:00 +0300

    feature: add board id routing

--- src/components/BoardCard/index.js
+++ src/components/BoardCard/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
@@ -7,7 +8,7 @@ const useStyles = makeStyles((theme) => ({
   board: {
     backgroundColor: theme.palette.secondary.main,
     backgroundClip: 'content-box',
-    minHeight: '200px',
+    minHeight: theme.spacing(25),
     transition: theme.customTransition,
     '&:hover': {
       backgroundColor: theme.palette.secondary.dark,
@@ -17,6 +18,13 @@ const useStyles = makeStyles((theme) => ({
   title: {
     color: 'white',
     margin: theme.spacing(2, 0, 0, 2),
+    paddingTop: theme.spacing(2),
+  },
+  routerLink: {
+    display: 'block',
+    textDecoration: 'none',
+    height: '100%',
+    marginTop: theme.spacing(-2),
   },
 }));
 
@@ -24,10 +32,20 @@ function BoardCard({ id, name }) {
   const classes = useStyles();
 
   return (
-    <Grid className={classes.board} component="li" item key={id} xs={12} sm={6} md={4}>
-      <Typography className={classes.title} component="h2" variant="h5">
-        {name}
-      </Typography>
+    <Grid
+      className={classes.board}
+      component="li"
+      item
+      key={id}
+      xs={12}
+      sm={6}
+      md={4}
+    >
+      <Link className={classes.routerLink} to={`/board/${id}`}>
+        <Typography className={classes.title} component="h2" variant="h5">
+          {name}
+        </Typography>
+      </Link>
     </Grid>
   );
 }
