884c86122a99f1a560000ce64b419a28f1bb3e91
FusionTech <fusion@fusion.com>
2021-03-31 11:01:50 +0300

    refactor: implement decomposition of large components

--- src/components/BoardDeletePopup/index.js
+++ src/components/BoardDeletePopup/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import Typography from '@material-ui/core/Typography';
-import Popover from '@material-ui/core/Popover';
-import Button from '@material-ui/core/Button';
 import { setBoards } from '../../store/boards';
-import useStyles from './style';
+import ConfirmPopup from '../ConfirmPopup';
 
 function BoardDeletePopup({
   id, delBoardId, isOpen, anchorEl, onClose,
 }) {
-  const classes = useStyles();
   const boards = useSelector((state) => state.boards.allBoards);
   const dispatch = useDispatch();
 
@@ -21,35 +17,15 @@ function BoardDeletePopup({
   };
 
   return (
-    <Popover
+    <ConfirmPopup
       id={id}
-      delBoardId={delBoardId}
-      open={isOpen}
+      delElId={delBoardId}
+      isOpen={isOpen}
       anchorEl={anchorEl}
+      btnText="Delete board"
       onClose={onClose}
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'left',
-      }}
-      transformOrigin={{
-        vertical: 8,
-        horizontal: 145,
-      }}
-      PaperProps={{ className: classes.paper }}
-    >
-      <Typography className={classes.title} component="h3" variant="h6">
-        Are you sure?
-      </Typography>
-      <Button
-        type="button"
-        variant="contained"
-        color="secondary"
-        fullWidth
-        onClick={() => deleteBoard(delBoardId)}
-      >
-        Delete board
-      </Button>
-    </Popover>
+      onClick={() => deleteBoard(delBoardId)}
+    />
   );
 }
 
